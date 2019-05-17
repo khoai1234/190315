84142ca1a29b1e3c55b093721cb18ec8d7052fee
khoaivn <trungdunght2003@gmail.com>
2019-05-17 10:00:02 +0700

    0. edit event to stand

--- Assets/SpawnController.cs
+++ Assets/SpawnController.cs
@@ -48,14 +48,15 @@ public class SpawnController : MonoBehaviour
                 int f1 = 3;
                 for (int i = 0; i < f1; i++)
                 {
-                    //GameObject PolA = Instantiate(police, spawnpointA.position, spawnpointA.rotation) as GameObject;
-                    GameObject PolA = Instantiate(police, new Vector3(hit.point.x + UnityEngine.Random.Range(1, 5),
-                         hit.point.y,
-                         hit.point.z + UnityEngine.Random.Range(1, 5)), transform.rotation) as GameObject;
+                    GameObject PolA = Instantiate(terrorist, new Vector3(hit.point.x + UnityEngine.Random.Range(1, 5),
+                                 hit.point.y,
+                                 hit.point.z + UnityEngine.Random.Range(1, 5)),
+                             transform.rotation) 
+                         as GameObject;
 
                 }
 
-                UnityEngine.Debug.Log("true" + hit.point);
+                //UnityEngine.Debug.Log("true" + hit.point);
             }
 
             //UnityEngine.Debug.Log(ray.dire);
@@ -64,60 +65,60 @@ public class SpawnController : MonoBehaviour
         //public int randomZ = UnityEngine.Random.Range(0, 2);
     }
 
-    public void OnClickEvent()
-    {
-        int f1, f2, f3, f4, f5;
+    //public void OnClickEvent()
+    //{
+    //    int f1, f2, f3, f4, f5;
         
-        //int randomX = UnityEngine.Random.Range(0, 2);
-        //int randomZ = UnityEngine.Random.Range(0, 2);
-        f1 = Convert.ToInt32(field1.text);
-        f2 = Convert.ToInt32(field2.text);
-        f3 = Convert.ToInt32(field3.text);
-        //SL khung bo
-        f4 = Convert.ToInt32(field4.text);
-        //SL con tim
-        f5 = Convert.ToInt32(field5.text);
-        //spawn police
-        for (int i = 0; i < f1; i++)
-        {
-            GameObject PolA = Instantiate(police, spawnpointA.position, spawnpointA.rotation) as GameObject;
-            //GameObject PolA = Instantiate(police, new Vector3 (spawnpointA.position.x + UnityEngine.Random.Range(0,2),0, spawnpointA.position.z + UnityEngine.Random.Range(0, 1)), spawnpointA.rotation) as GameObject;
-        }
+    //    //int randomX = UnityEngine.Random.Range(0, 2);
+    //    //int randomZ = UnityEngine.Random.Range(0, 2);
+    //    f1 = Convert.ToInt32(field1.text);
+    //    f2 = Convert.ToInt32(field2.text);
+    //    f3 = Convert.ToInt32(field3.text);
+    //    //SL khung bo
+    //    f4 = Convert.ToInt32(field4.text);
+    //    //SL con tim
+    //    f5 = Convert.ToInt32(field5.text);
+    //    //spawn police
+    //    for (int i = 0; i < f1; i++)
+    //    {
+    //        GameObject PolA = Instantiate(police, spawnpointA.position, spawnpointA.rotation) as GameObject;
+    //        //GameObject PolA = Instantiate(police, new Vector3 (spawnpointA.position.x + UnityEngine.Random.Range(0,2),0, spawnpointA.position.z + UnityEngine.Random.Range(0, 1)), spawnpointA.rotation) as GameObject;
+    //    }
 
-        for (int i = 0; i < f2; i++)
-        {
-            GameObject PolB = Instantiate(police, spawnpointB.position, spawnpointB.rotation) as GameObject;
-            //GameObject PolB = Instantiate(police, new Vector3(spawnpointB.position.x + UnityEngine.Random.Range(0, 2), 0, spawnpointB.position.z + UnityEngine.Random.Range(0, 1)), spawnpointB.rotation) as GameObject;
-        }
+    //    for (int i = 0; i < f2; i++)
+    //    {
+    //        GameObject PolB = Instantiate(police, spawnpointB.position, spawnpointB.rotation) as GameObject;
+    //        //GameObject PolB = Instantiate(police, new Vector3(spawnpointB.position.x + UnityEngine.Random.Range(0, 2), 0, spawnpointB.position.z + UnityEngine.Random.Range(0, 1)), spawnpointB.rotation) as GameObject;
+    //    }
 
-        for (int i = 0; i < f3; i++)
-        {
-            GameObject PolC = Instantiate(police, spawnpointC.position, spawnpointC.rotation) as GameObject;
-            //GameObject PolC = Instantiate(police, new Vector3(spawnpointC.position.x + UnityEngine.Random.Range(0, 1), 0, spawnpointC.position.z + UnityEngine.Random.Range(0, 1)), spawnpointC.rotation) as GameObject;
-        }
+    //    for (int i = 0; i < f3; i++)
+    //    {
+    //        GameObject PolC = Instantiate(police, spawnpointC.position, spawnpointC.rotation) as GameObject;
+    //        //GameObject PolC = Instantiate(police, new Vector3(spawnpointC.position.x + UnityEngine.Random.Range(0, 1), 0, spawnpointC.position.z + UnityEngine.Random.Range(0, 1)), spawnpointC.rotation) as GameObject;
+    //    }
 
-        for (int i = 0; i < f4; i++)
-        {
-            GameObject Terr = Instantiate(terrorist, spawnpointD.position, spawnpointD.rotation) as GameObject;
-            //GameObject Terr = Instantiate(terrorist, new Vector3(spawnpointD.position.x + UnityEngine.Random.Range(0, 1), 0, spawnpointD.position.z + UnityEngine.Random.Range(0, 1)), spawnpointD.rotation) as GameObject;
-        }
+    //    for (int i = 0; i < f4; i++)
+    //    {
+    //        GameObject Terr = Instantiate(terrorist, spawnpointD.position, spawnpointD.rotation) as GameObject;
+    //        //GameObject Terr = Instantiate(terrorist, new Vector3(spawnpointD.position.x + UnityEngine.Random.Range(0, 1), 0, spawnpointD.position.z + UnityEngine.Random.Range(0, 1)), spawnpointD.rotation) as GameObject;
+    //    }
 
-        for (int i = 0; i < f5; i++)
-        {
-            int x = UnityEngine.Random.Range(1, 2);
-            if (x == 1)
-            {
-                GameObject Hosta1 = Instantiate(hostage1, spawnpointE.position, spawnpointE.rotation) as GameObject;
-                Debug.Log("1");
-            }
-            if (x == 2)
-            {
-                GameObject Hosta2 = Instantiate(hostage2, spawnpointF.position, spawnpointF.rotation) as GameObject;
-                Debug.Log("2");
-            }
-            x = 2;
-            //GameObject PolA = Instantiate(police, spawnpointA.position, spawnpointA.rotation) as GameObject;
-        }
+    //    for (int i = 0; i < f5; i++)
+    //    {
+    //        int x = UnityEngine.Random.Range(1, 2);
+    //        if (x == 1)
+    //        {
+    //            GameObject Hosta1 = Instantiate(hostage1, spawnpointE.position, spawnpointE.rotation) as GameObject;
+    //            Debug.Log("1");
+    //        }
+    //        if (x == 2)
+    //        {
+    //            GameObject Hosta2 = Instantiate(hostage2, spawnpointF.position, spawnpointF.rotation) as GameObject;
+    //            Debug.Log("2");
+    //        }
+    //        x = 2;
+    //        //GameObject PolA = Instantiate(police, spawnpointA.position, spawnpointA.rotation) as GameObject;
+    //    }
 
-    }
+    //}
 }
